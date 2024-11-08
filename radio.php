200ca526829b59f167bf2ac8193b239cff7da511
techcr8tive <149272062+techcr8tive@users.noreply.github.com>
2024-11-08 20:31:27 +0100

    Add files via upload

---
+++ radio.php
@@ -0,0 +1,277 @@
+ <?php
+echo '
+<!doctype html>
+<html lang="zxx">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title> Live Radio | Christ Embassy Country Home</title>
+  <!-- Template CSS -->
+  <link href="//fonts.googleapis.com/css?family=Poppins:300,400,400i,500,600,700&display=swap" rel="stylesheet">
+  <link href="//fonts.googleapis.com/css2?family=Limelight&display=swap" rel="stylesheet">
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="assets/css/style-starter.css">
+  <!-- Template CSS -->
+</head>
+
+<body>
+  <!--header-->
+  <header id="site-header" class="fixed-top">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <h1><a class="navbar-brand" href="http://cecountryhome.com">
+            <img src="assets/images/ce.png" alt="Your logo" title="Your logo" style="height:35px;" />
+          </a></h1>
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="fa icon-expand fa-bars"></span>
+          <span class="fa icon-close fa-times"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="salvation.php">Salvation</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="livetv.php">Live Stream</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="radio.php">Live Radio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="videos.php">Videos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="give.php">Give</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav search-right mt-lg-0 mt-2">
+            <li class="nav-item mr-3" title="Search"><a href="#search" class="btn search-search">
+                <span class="fa fa-search" aria-hidden="true"></span></a></li>
+            <li class="nav-item"><a href="https://chat.whatsapp.com/KDE1W1h8Oqh1zJvwS4Cole" class="btn btn-primary d-none d-lg-block btn-style mr-2">Contact
+                Us</a></li>
+          </ul>
+
+          <!-- //toggle switch for light and dark theme -->
+          <!-- search popup -->
+          <div id="search" class="pop-overlay">
+            <div class="popup">
+              <form action="#" method="GET" class="d-sm-flex">
+                <input type="search" placeholder="Search.." name="search" required="required" autofocus>
+                <button type="submit">Search</button>
+                <a class="close" href="#url">&times;</a>
+              </form>
+            </div>
+          </div>
+          <!-- /search popup -->
+        </div>
+        <!-- toggle switch for light and dark theme -->
+        <div class="mobile-position">
+          <nav class="navigation">
+            <div class="theme-switch-wrapper">
+              <label class="theme-switch" for="checkbox">
+                <input type="checkbox" id="checkbox">
+                <div class="mode-container">
+                  <i class="gg-sun"></i>
+                  <i class="gg-moon"></i>
+                </div>
+              </label>
+            </div>
+          </nav>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <!--/header-->
+   <!-- about breadcrumb -->
+   <section class="w3l-about-breadcrumb position-relative text-center">
+    <div class="breadcrumb-bg breadcrumb-bg-about py-sm-5 py-4">
+      <div class="container py-lg-5 py-3">
+        <h2 class="title">Live Radio</h2>
+        <ul class="breadcrumbs-custom-path mt-2">
+          <li><a href="#url">Home</a></li>
+          <li class="active"><span class="fa fa-angle-double-right mx-2" aria-hidden="true"></span> live Radio</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <!-- //about breadcrumb -->
+
+
+  <!-- /content-6-->
+	<section class="w3l-content-6">
+		<!-- /content-6-main-->
+		                    <div class="content-6-mian py-5">
+			<div class="container py-lg-5">
+				<div class="content-info-in row">
+					<div class="col-lg-6">
+						<img src="" alt="" class="img-fluid">
+					</div>
+					<div class="col-lg-6 mt-lg-0 mt-5 about-right-faq align-self  pl-lg-4">
+						<div class="title-content text-left mb-2">
+							<h6 class="sub-title"><b>ATTENTION !!!</b></h6>
+							<h3 class="hny-title">ONLINE Radio AND LIVE SERVICE</h3>
+						</div>						<p class="mt-3">Online Radio platform designed to deliver fresh, wholesome, faith-stirring and inspiring programming content; ultimately channeled at building lives with God’s eternal Word.
+</p> </div> </section>
+	<!-- //content-6-->
+	<!-- /w3l-content-4 -->
+	<section class="w3l-content-4">
+		<!-- /content-6-section -->
+		 
+
+
+		
+		
+<link rel="stylesheet" href="css/radio.css">
+<script src="js/radio.js"></script>
+
+<center><div id="stream-info" class="hidden"></div>
+<audio class="op-player__media" id="audio" controls>
+  <source src="https://cdn-out1-los1.ceflixcdn.com/pcdltvapp/pcdltv/chunklist.m3u8">
+</audio></center>
+<br>
+<br>
+<br>
+	</section>
+	<!-- //content-6-section -->
+
+	<!--//content-5-->
+
+	<!--//content-5-->
+
+	<!-- team -->
+	  <!-- footer-66 -->
+  <footer class="w3l-footer-66">
+    <section class="footer-inner-main">
+      <div class="footer-hny-grids py-5">
+        <div class="container py-lg-4">
+          <div class="text-txt">
+            
+            <div class="right-side">
+              <div class="row sub-columns">
+                <div class="col-lg-4 col-md-6 sub-one-left pr-lg-4">
+                  
+                  <!-- if logo is image enable this   
+										<a class="navbar-brand" href="#index.html">
+											<img src="http://christembassyantwerp.be/wp-content/uploads/2015/12/ceorg_logo-white-300x150.png" alt="Your logo" title="Your logo" style="height:35px;" />
+										</a> -->
+                  <p class="pr-lg-4">Christ Embassy is not just a local assembly; it’s a vision. The Lord has called us to 
+fulfill a very definite purpose, which is to take His divine presence to the peoples and nations of the world, 
+and to demonstrate the character of His Spirit everywhere</p>
+                  <div class="columns-2">
+                    <ul class="social">
+                      <li><a href="https://www.facebook.com/cecountryhome"><span class="fa fa-facebook" aria-hidden="true"></span></a>
+                      </li>
+                      <li><a href="https://www.instagram.com/cechpage"><span class="fa fa-instagram" aria-hidden="true"></span></a>
+                      </li>
+                      <li><a href="https://chat.whatsapp.com/KDE1W1h8Oqh1zJvwS4Cole"><span class="fa fa-whatsapp" aria-hidden="true"></span></a>
+                      </li>
+                      
+                    </ul>
+                  </div>
+                </div>
+                                <div class="col-lg-4 col-md-6 sub-one-left">
+                  <h6>Contact Info</h6>
+                  <div class="sub-contact-info">
+                    <p>Address: 3 Igbinedion Street, Off Country Home Road, G.R.A Sapele Road, Benin City.</p>
+                    <p class="my-3">Phone: <strong><a href="tel:+234 816 431 0990">+234 816 431 0990</a></strong></p>
+                    <p>E-mail:<strong> <a href="mailto:media@cecountryhome.com">media@cecountryhome.com</a></strong></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="below-section">
+        <div class="container">
+          <div class="copyright-footer">
+            <div class="columns text-lg-left">
+              <p>© 2023 All rights reserved | Designed by <a href="index.php">Cr8tive Hub</a></p>
+            </div>
+            
+      <!-- copyright -->
+      <!-- move top -->
+      <button onclick="topFunction()" id="movetop" title="Go to top">
+        <span class="fa fa-long-arrow-up" aria-hidden="true"></span>
+      </button>
+      <script>
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function () {
+          scrollFunction()
+        };
+
+        function scrollFunction() {
+          if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("movetop").style.display = "block";
+          } else {
+            document.getElementById("movetop").style.display = "none";
+          }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+          document.body.scrollTop = 0;
+          document.documentElement.scrollTop = 0;
+        }
+      </script>
+      <!-- /move top -->
+
+    </section>
+  </footer>
+  <!--//footer-66 -->
+  <!-- Template JavaScript -->
+  <script src="assets/js/jquery-3.3.1.min.js"></script>
+  <script src="assets/js/theme-change.js"></script>
+  <!-- disable body scroll which navbar is in active -->
+  <script>
+    $(function () {
+      $(\'.navbar-toggler\').click(function () {
+        $(\'body\').toggleClass(\'noscroll\');
+      })
+    });
+  </script>
+  <!-- disable body scroll which navbar is in active -->
+  <!--/MENU-JS-->
+  <script>
+    $(window).on("scroll", function () {
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 80) {
+        $("#site-header").addClass("nav-fixed");
+      } else {
+        $("#site-header").removeClass("nav-fixed");
+      }
+    });
+
+    //Main navigation Active Class Add Remove
+    $(".navbar-toggler").on("click", function () {
+      $("header").toggleClass("active");
+    });
+    $(document).on("ready", function () {
+      if ($(window).width() > 991) {
+        $("header").removeClass("active");
+      }
+      $(window).on("resize", function () {
+        if ($(window).width() > 991) {
+          $("header").removeClass("active");
+        }
+      });
+    });
+  </script>
+  <!--//MENU-JS-->
+
+  <script src="assets/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
+
+';
+?>
